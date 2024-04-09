2782174aaae932ea46bd5d9c4f5c2ac1f57f4a80
Kevin <mrkev63@gmail.com>
2024-04-09 17:44:41 -0400

    finished q1-4

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,16 +1,32 @@
-const addToFrontOrBack = () => {
+const addToFrontOrBack = (arr, value, isFront) => {
+  if (isFront === true) {
+    arr.unshift(value)
+    return arr
+  } else {
+    arr.push(value)
+    return arr
+  }
 };
 
-const reverseString = () => {
+const reverseString = (str) => {
+  let arr = str.split('').reverse().join('');
+  return arr
+}
+
+const newArrayFullOf = (value, numOfVal) => {
+  let arr = new Array(numOfVal)
+  return arr.fill(value)
 };
 
-const newArrayFullOf = () => {
+const insertIntoMiddle = (arr, value) => {
+  //let middle = arr[Math.floor(arr.length / 2)];
+  arr.splice(Math.floor(arr.length / 2), 0, value)
+  return arr
 };
 
-const insertIntoMiddle = () => {
-};
-
-const deleteFromMiddle = () => {
+const deleteFromMiddle = (arr) => {
+  arr.splice(Math.ceil(arr.length / 2) - 1, 1)
+  return arr
 };
 
 const isRightIndex = () => {
--- src/playground.js
+++ src/playground.js
@@ -1,10 +1,61 @@
 // const { clearArr, getFirstItem } = require('./debug');
 // const {
 //   addToFrontOrBack,
+
+const addToFrontOrBack = (arr, value, isFront) => {
+    //let front = true
+    //let arr1 = []
+    if (isFront === true) {
+        arr.unshift(value)
+        return arr
+    } else {
+        arr.push(value)
+        return arr
+    }
+    // arr.push(value)
+    // console.log(arr)
+};
+//console.log(addToFrontOrBack([1, 2, 3, 4], 6, false))
+
 //   reverseString,
+
+const reverseString = (str) => {
+    let arr = str.split('').reverse().join('');
+    //console.log(arr)
+    // let reverse = arr.reverse();
+    // console.log(reverse)
+    return arr
+
+
+}
+//console.log(reverseString('hello, Hi'))
+
 //   newArrayFullOf,
+
+const newArrayFullOf = (value, numOfVal) => {
+    let arr = new Array(numOfVal)
+    return arr.fill(value)
+}
+//console.log(newArrayFullOf(true, 3))
+
 //   insertIntoMiddle,
+
+const insertIntoMiddle = (arr, value) => {
+    //let middle = arr[Math.floor(arr.length / 2)];
+    arr.splice(Math.floor(arr.length / 2), 0, value)
+    return arr
+};
+//console.log(insertIntoMiddle(['a', 'b', 'c', 'd'], 'z'))
 //   deleteFromMiddle,
+
+const deleteFromMiddle = (arr) => {
+    arr.splice(Math.ceil(arr.length / 2), 1)
+    return arr
+};
+console.log(deleteFromMiddle(['a', 'v', 'c', 'd']))
+console.log(deleteFromMiddle([1, 2, 3]))
+console.log(deleteFromMiddle([1, 2, 3, 4, 5]))
+
 //   isRightIndex,
 //   roundAllNumsDown,
 //   getAllYCoordinates,
